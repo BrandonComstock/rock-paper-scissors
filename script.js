72355237d75efe39f780edf829405149215079bd
BrandonComstock <brandoncomstock40@gmail.com>
2025-04-24 18:45:48 -0500

    added logic to playRound fucntion, made switch statements for what human and computer choose

--- script.js
+++ script.js
@@ -15,7 +15,49 @@ function getHumanChoice () {
 }
 
 function playRound(humanChoice, computerChoice) {
-
+    let message;
+    switch (humanChoice) {
+        case "ROCK": {
+            switch (computerChoice) {
+                case "rock": {
+                    message = "Tie! try again";
+                }
+                case "paper": {
+                    message = "You lose! Paper beats rock";
+                }
+                case "scissors": {
+                    message = "You win! Rock beats scissors";
+                }
+            }
+        }
+        case "PAPER": {
+            switch (computerChoice) {
+                case "rock": {
+                    message = "You win! Paper beats rock";
+                }
+                case "paper": {
+                    message = "Tie! try again";
+                }
+                case "scissors": {
+                    message = "You lose! Scissors beats paper";
+                }
+            }
+        }
+        case "SCISSORS": {
+            switch (computerChoice) {
+                case "rock": {
+                    message = "You lose! Rock beats scissors";
+                }
+                case "paper": {
+                    message = "You win! Scissors beats paper";
+                }
+                case "scissors": {
+                    message = "Tie! try again";
+                }
+            }
+        }
+    }
+    console.log(message);
 }
 
 const humanSelection = getHumanChoice();
